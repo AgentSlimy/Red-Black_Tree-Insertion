f8bf9b12e823511d15faa2ef2aa6770d2d2ec580
U-BSD\417197 <417197@SHS-10D9C06E3.beaverton.k12.or.us>
2022-04-27 11:51:25 -0700

    Manual add

--- main.cpp
+++ main.cpp
@@ -6,8 +6,13 @@
 using namespace std;
 
 void PARSE();
-void ADD();
+void ADD(Node* &head, Node* &current, Node*& previous, int value);
+void FADD();
 void PRINT();
+
+void BALANCE();
+void rotateLeft();
+void rotateRight();
 
 int main() {
   cout << "Red-Black Tree: Insertion" << endl << endl;
@@ -22,21 +27,62 @@ int main() {
     cin.get(command, 15);
     cin.clear();
     cin.ignore(10000, '\n');
-    if (strcmp(command, "Add") == 0) {
+    if (strcmp(command, "Add") == 0) { //Add command, manually add a single value to the tree
+      
+    }
+    else if (strcmp(command, "FAdd") == 0) { //File add command, add values from a file to the tree
 
     }
-    else if (strcmp(command, "FAdd") == 0) {
+    else if (strcmp(command, "Print") == 0) { //Print command, print out tree
 
     }
-    else if (strcmp(command, "Print") == 0) {
-
-    }
-    else if (strcmp(command, "Quit") == 0) {
+    else if (strcmp(command, "Quit") == 0) { //Quit command, quits
       cout << endl << "Quitting Red-Black Tree: Insertion Edition" << endl;
       running = false;
     }
     else {
       cout << "Invalid input, try again." << endl << endl;
+    }
+  }
+}
+
+void ADD(Node* &head, Node* &current, Node*& previous, int value) {
+  if (head == NULL) {
+    head = new Node();
+    current = head;
+    head->setData(value);
+    head->setColor(0); //Head is always black
+  }
+  else {
+    if (value < current->getData()) {
+      previous = current;
+      current = current->getLeft();
+      if (current == NULL) {
+	current = new Node();
+	current->setData(value);
+	previous->setLeft(current);
+	current->setParent(previous);
+	//balance
+	return;
+      }
+      else {
+	ADD(head, current, previous, value);
+      }
+    }
+    else {
+      previous = current;
+      current = current->getRight();
+      if (current == NULL) {
+	current = new Node();
+	current->setData(value);
+	previous->setRight(current);
+	current->setParent(previous);
+	//balance
+	return;
+      }
+      else {
+	ADD(head, current, previous, value);
+      }
     }
   }
 }
